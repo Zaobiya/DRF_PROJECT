ca91786eef5c2614e1bdefc06d1dfaac2aeadd51
Zaobiya <zaobiyakhan3@gmail.com>
2023-08-26 15:25:29 +0530

    commit:14

--- watchmate/myappone/urls.py
+++ watchmate/myappone/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path
-from myappone import views
+# from django.urls import path
+# from myappone import views
 
-urlpatterns = [
-    path('list/', views.movie_list, name='movie_list'),
-    path('<int:pk>/', views.movie_details, name='movie_detail'),
-]
+# urlpatterns = [
+#     path('list/', views.movie_list, name='movie_list'),
+#     path('<int:pk>/', views.movie_details, name='movie_detail'),
+# ]
